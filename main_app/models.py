8d1416774c6618f560f14a824660ce6e32c0b86a
unknown <mohammed98ali.ma@gmail.com>
2025-09-02 13:23:54 +0300

    first commit. class meta is left for the model

---
+++ main_app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+    class Meta:
+        pass
+
+
+class Move(models.Model):
+    Guard_choices = [
+        ('Low', 'Low'),
+        ('Mid', 'Mid'),
+        ('High', 'High')
+        ]
+    move_choices = [
+        ('Normal','Special'),
+        ('Normal', 'Special')
+    ]
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name ='move')
+    name = models.CharField(max_length=50)
+    move_type = models.CharField(
+        max_length=50,
+        choices= move_choices,
+        default='Normal'
+        )
+    damage = models.CharField(max_length=10)
+    guard_type = models.CharField(
+        max_length=20,
+        choices= Guard_choices,
+        default='Mid'
+    )
+    start_up = models.IntegerField()
+    active = models.IntegerField()
+    recovery = models.IntegerField()
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f'{self.character} - {self.name}'
+
+    class Meta:
+        pass
